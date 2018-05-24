4bc3f99abefe08983c557cbe7a20ab3c13607fc0
James Simpson <jamessimpson17gsu@gmail.com>
2018-05-24 03:08:46 +0000

    Hello.rb

---
+++ hello.rb
@@ -0,0 +1,10 @@
+  gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+  puts "a visual prothesis #{gift.sample}."
